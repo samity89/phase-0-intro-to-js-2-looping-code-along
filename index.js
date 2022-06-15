0b7d6d14dec266091b7dbb866f0f8bfe0a54cd3d
Samuel Amity <sam.amity@gmail.com>
2022-06-15 12:20:49 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards (name, event){
+    let card = [];
+    for (let i = 0; i < name.length; i++) {
+        card.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return card;
+};
+
+function countDown(int) {
+    let i = 10
+    while (i >= 0) {
+    console.log(i);
+    i--;
+    }
+};
